e20f87204d16a2ddb5b84554a20e78616de1136b
碎碎酱 <yinxin630@gmail.com>
2015-11-04 18:30:35 +0800

    临时同步

--- api/controllers/SessionController.js
+++ api/controllers/SessionController.js
@@ -7,11 +7,18 @@
 
 module.exports = {
     create: function (req, res) {
-        
+        var session = req.session;
+        SessionService.create(session, res);
     },
     
     destroy: function (req, res) {
-        
+        var session = req.session;
+        SessionService.destroy(session, res);
+    },
+    
+    find: function (req, res) {
+        var session = req.session || undefined;
+        res.ok({ session: session });
     }
 };
 
---
+++ api/services/SessionService.js
@@ -0,0 +1,11 @@
+module.exports = {
+    var loggedUser = [],
+    
+    create: function (session, res) {
+        
+    },
+    
+    destroy: function (session, res) {
+        
+    }
+}
