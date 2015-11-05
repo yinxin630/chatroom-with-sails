4d904535da81922fd1fc8d2bfcf030cfe7858f0a
碎碎酱 <yinxin630@gmail.com>
2015-11-05 12:27:07 +0800

    实现Message方法，修改主页

--- api/controllers/MessageController.js
+++ api/controllers/MessageController.js
@@ -7,15 +7,26 @@
 
 module.exports = {
     create: function (req, res) {
+        if (!req.isSocket) {
+            res.badRequest('please request this interface with socket.');
+        }
+        if (JSON.stringify(sails.sockets.socketRooms(req.socket)) != 'default') {
+            res.badRequest('you had not joined room.');
+        }
         var msg = req.param('msg');
-        var session = req.session;
-        sails.log.info(session.user);
-        sails.log.info(msg);
-        res.ok();
+        sails.boradcast('default', 'message', { msg: msg });
+        res.ok('send message success.');
     },
     
     find: function (req, res) {
-        
+        if (!req.isSocket) {
+            res.badRequest('please request this interface with socket');
+        }
+        if (JSON.stringify(sails.sockets.socketRooms(req.socket)) == 'default') {
+            res.badRequest('you had joined room');
+        }
+        sails.sockets.join(req.socket, 'default');
+        res.ok('join room success.');
     }
 };
 
