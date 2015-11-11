5886b0f1c499ffd0de4bbc2b1301b5225f4628cf
碎碎酱 <yinxin630@gmail.com>
2015-11-11 10:48:11 +0800

    修改项目部署是端口配置，本地测试时仍未默认1337端口

--- config/env/production.js
+++ config/env/production.js
@@ -25,7 +25,7 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  port: 50001,
+  port: 10086,
 
   /***************************************************************************
    * Set the log level in production environment to "silent"                 *
