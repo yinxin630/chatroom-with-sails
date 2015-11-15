2cc7979549640e0b83b1dc07f15fd14ac219651f
碎碎酱 <yinxin630@gmail.com>
2015-11-15 14:45:51 +0800

    修改提示文本颜色

--- api/util/ResponseInfo.js
+++ api/util/ResponseInfo.js
@@ -4,5 +4,10 @@ module.exports = {
 	DESTROY_SESSION_SUCCESS: 'Destroy session success.',
 	GET_SESSION_SUCCESS: 'Get session success.',
 	SESSION_NOT_EXISTS: 'Session not exists.',
+	USE_SOCKET: 'Please use the socket to access this interface.',
+	NICK_MODIFIED_SUCCESS: 'Nick has been modified.',
+	NOT_JOINED_ROOM: 'You have not joined the room.',
+	SEND_MESSAGE_SUCCESS: 'Send message success.',
+	JOIN_ROOM_SUCCESS: 'Join room success.',
 	
 }
