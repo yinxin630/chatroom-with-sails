ad74148b5a60e5d940c3242c2920f07f86fcde77
碎碎酱 <yinxin630@gmail.com>
2015-11-17 17:36:06 +0800

    添加socket model

---
+++ api/controllers/SocketController.js
@@ -0,0 +1,11 @@
+/**
+ * SocketController
+ *
+ * @description :: Server-side logic for managing sockets
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+};
+
