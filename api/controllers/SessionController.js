d59bd75518f55b991f243ab867d8db8768156b3f
碎碎酱 <yinxin630@gmail.com>
2015-12-03 12:22:59 +0800

    添加Session model

---
+++ api/controllers/SessionController.js
@@ -0,0 +1,11 @@
+/**
+ * SessionController
+ *
+ * @description :: Server-side logic for managing sessions
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
